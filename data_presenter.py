ff57a08f150336130bd1842666fc2b559307cc1b
Misha Martin <mishanicolemartin@gmail.com>
2021-10-04 17:02:07 -0600

    Final Commit

--- data_presenter.py
+++ data_presenter.py
@@ -3,9 +3,9 @@ cupcake_file = open("CupcakeInvoices.csv")
 # for line in cupcake_file:
 #     print(line)
 
-# for line in cupcake_file:
-#     line_split = line.rstrip("\r\n").split(",")
-#     print(line_split[2])
+for line in cupcake_file:
+    line_split = line.rstrip("\r\n").split(",")
+    print(line_split[2])
 
 # for line in cupcake_file:
 #     line_split = line.rstrip("\r\n").split(",")
@@ -30,4 +30,11 @@ print (sum)
 # example = 76.3656985
 # a_round = round(example, 2)
 # print(a_round)
+cupcake_file.close()
 
+#this is a text for matplotlib. Please use python3 or pip3 if you need to access that. 
+#python3 {name of file} in the terminal will make sure that it is going to show a graph with the formula below.
+# import matplotlib.pyplot as plt
+
+# plt.bar([1,2,3],[4,5,6])
+# plt.show()
